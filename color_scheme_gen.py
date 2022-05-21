9a074423d558018f7869bfbbe98a4d031b91361e
steven060930 <84993331+steven060930@users.noreply.github.com>
2022-05-21 15:28:01 -0400

    Update color_scheme_gen.py

--- color_scheme_gen.py
+++ color_scheme_gen.py
@@ -1,2 +1,1 @@
-import random
-print(["#"+''.join([random.choice('0123456789ABCDEF') for i in range(6)]) for _ in range(int(input()))])
+print(["#"+''.join([__import__('random').choice('0123456789ABCDEF') for i in range(6)]) for _ in range(50)])
